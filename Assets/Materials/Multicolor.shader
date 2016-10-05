f10c00d8f0404daad47f4e55e973a5a8db8ebb67
Jose Moreno <josem1988@gmail.com>
2016-10-05 21:15:05 +0200

    First commit!

---
+++ Assets/Materials/Multicolor.shader
@@ -0,0 +1,56 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Unlit/Multicolor"
+{
+	Properties
+	{
+		_Color1 ("Color 1", Color) = (0,0,0,0) 
+		_Color2 ("Color 2", Color) = (0,0,0,0)
+		_Color3 ("Color 3", Color) = (0,0,0,0) 
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 worldPos : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			float4 _Color1;
+			float4 _Color2;
+			float4 _Color3;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.worldPos = mul(unity_ObjectToWorld,v.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = lerp(_Color1,_Color2,(i.worldPos.y+0.5)/0.5);
+				col = lerp(_Color2,_Color3,i.worldPos.y/0.5);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Scripts/SimpleMeshGenerator.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMeshGenerator : MonoBehaviour {
+
+	public int N;
+	public float width;
+	public float longWidth;
+	public float timeFactor;
+
+	Mesh _mesh;
+	Vector3[] _vs;
+	int[] _inds;
+
+	void Start () {
+
+		_vs = new Vector3[2*N];
+		_inds = new int[6*(N-1)];
+
+		int k=0;
+		for(int i=0;i<N-1;i++){
+
+			_inds[0+k] = 2*i;
+			_inds[1+k] = 2*i+1;
+			_inds[2+k] = 2*(i+1)+1;
+			_inds[3+k] = 2*(i+1)+1;
+			_inds[4+k] = 2*(i+1);
+			_inds[5+k] = 2*i;
+
+			k+=6;
+		}
+
+
+		_mesh = new Mesh();
+
+		GenerateMesh();
+
+		_mesh.SetIndices(_inds,MeshTopology.Triangles,0);
+
+		GetComponent<MeshFilter>().mesh = _mesh;
+
+	}
+
+	void GenerateMesh(){
+		for(int i=0;i<N;i=i+1){
+
+			Vector3 c = Curve(longWidth*1f*i/N);
+			Vector3 corto = DerivateCurve(longWidth*1f*i/N,longWidth*1f/N);
+			corto = OrtoDerivateCurve(corto);
+
+			_vs[2*i] = c - width*corto.normalized;
+			_vs[2*i+1] = c + width*corto.normalized;
+
+		}
+
+		_mesh.vertices = _vs;
+
+
+	}
+
+	void Update(){
+		GenerateMesh();
+	}
+
+	public Vector3 Curve(float t){
+
+		t = t+timeFactor*Time.time;
+
+		float xt = Mathf.Cos(5f*t)+3f*Mathf.Sin(4f*t);
+		float yt = 3f*Mathf.Sin(3f*t)+2f*Mathf.Sin(5f*t);
+		return new Vector3(xt,yt,0f);
+
+	}
+
+	public Vector3 DerivateCurve(float t,float dt){
+
+		return (Curve(t+dt)-Curve(t))/dt;
+
+	}
+
+	public Vector3 OrtoDerivateCurve(Vector3 v){
+
+		Vector3 vorto = v;
+		vorto.y = v.x;
+		vorto.x = -v.y;
+
+		return vorto;
+
+	}
+	
+
+}
